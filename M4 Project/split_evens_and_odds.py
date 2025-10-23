1cb3db287913b32056500e12c2b152f90b5481d8
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-10-23 08:19:59 -0400

    completed split evens/odds class definition

--- M4 Project/split_evens_and_odds.py
+++ M4 Project/split_evens_and_odds.py
@@ -8,7 +8,24 @@ class SplitEvensOdds(SinglyLinkedList):
         evens = SinglyLinkedList()
         odds = SinglyLinkedList()
         
+        cur = self.head
+        self.head = self.tail = None
+        self.count = 0
 
+        while cur:
+            nxt = cur.next
+            cur.next = None
+            target = evens if (isinstance(cur.data, int) and cur.data % 2 == 0) else odds
+            if target.tail is None:
+                target.head = target.tail = cur
+                target.count = 1
+            else:
+                target.tail.next = cur
+                target.tail = cur
+                target.count += 1
+            cur = nxt
+
+        return evens, odds
 
 
 
