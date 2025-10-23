21cffa60c856dde79b7c73ac06c094ef50ce05d4
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-10-23 19:22:15 -0400

    updated misaligned class calls

--- M4 Project/main.py
+++ M4 Project/main.py
@@ -1,12 +1,12 @@
 #M4 Projects Split Evens and Odds
 
-from split_evens_odds import SplitEvensOdds
+from split_evens_and_odds import SplitEvensOdds
 
 def run_demo():
     ll = SplitEvensOdds()
-    ll.insertAtEnd([1,2,3,4,5,6,7,8,15,14,13,12,11,10,9])
+    ll.insert_at_end([1,2,3,4,5,6,7,8,15,14,13,12,11,10,9])
     print(ll.display())
-    ev, od = ll.split_even_odd()
+    ev, od = ll.split_evens_and_odds()
     print(ev.display())
     print(od.display())
     print(ll.display())
--- M4 Project/split_evens_and_odds.py
+++ M4 Project/split_evens_and_odds.py
@@ -4,7 +4,7 @@ from singly_linked_list import SinglyLinkedList
 from node import Node
 
 class SplitEvensOdds(SinglyLinkedList):
-    def split_even_odd(self) -> Tuple[SinglyLinkedList, SinglyLinkedList]:
+    def split_evens_and_odds(self) -> Tuple[SinglyLinkedList, SinglyLinkedList]:
         evens = SinglyLinkedList()
         odds = SinglyLinkedList()
         
