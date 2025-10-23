c8d0a05f051dadd766bbbfb76767e1437eac11f2
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-10-23 07:23:25 -0400

    Added main test script to build, display, and modify a singly linked list for build forward, delete first/last, and delete value

--- M4 Project/main.py
+++ M4 Project/main.py
@@ -1,2 +1,20 @@
 #M4 Projects Singly Linked List
 
+from singly_linked_list import SinglyLinkedList
+
+def run_tests():
+    print("Build a Forward List")
+    ll = SinglyLinkedList()
+    ll.build_list_forward([10, 20, 30, 40, 50])
+    print(ll.display())
+
+    ll.delete_first()
+    print("Delete the First Node:", ll.display())
+
+    ll.delete_last()
+    print("Delete the Last Node:", ll.display())
+
+    ll.delete_value(30)
+    print("Delete the Interior Node:", ll.display())
+
+    
