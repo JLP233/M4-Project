33b610ea835be86a12366af646379a408163a824
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-10-23 08:00:35 -0400

    added even/odds class

---
+++ M4 Project/split_evens_and_odds.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from typing import Tuple
+from singly_linked_list import SinglyLinkedList
+from node import Node
+
+class SplitEvensOdds(SinglyLinkedList):
+    def split_even_odd(self) -> Tuple[SinglyLinkedList, SinglyLinkedList]:
+        evens = SinglyLinkedList()
+        odds = SinglyLinkedList()
+        
+
+
+
+
+
